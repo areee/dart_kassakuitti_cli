9c97ef267f8f0bf33553b0e56ce4292020f8723f
areee <10089872+areee@users.noreply.github.com>
2021-11-13 14:58:13 +0200

    Refactor html to csv generating into own file

--- bin/dart_kassakuitti_cli.dart
+++ bin/dart_kassakuitti_cli.dart
@@ -1,14 +1,9 @@
-import 'load_html.dart';
+import 'read_html_and_save_as_csv.dart';
 import 'read_receipt_and_save_as_csv.dart';
-import 'save_eps_as_csv.dart';
 
-void main(List<String> arguments) async {
+void main(List<String> arguments) {
   readReceiptAndSaveAsCSV('assets/files/_cashReceipt.txt');
-
-  var awaitedEANProductList =
-      await loadHtmlFromAssets('assets/files/_orderedProducts.html');
-
-  eanProductListToCSV(awaitedEANProductList);
+  readHtmlAndSaveAsCSV('assets/files/_orderedProducts.html');
 
   print('Done!');
 }
