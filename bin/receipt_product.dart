ffed449317760258a0d7de4c695cd2d6554ea9dc
areee <10089872+areee@users.noreply.github.com>
2021-11-20 18:19:47 +0200

    Add 'discountCounted' variable into ReceiptProduct

--- bin/receipt_product.dart
+++ bin/receipt_product.dart
@@ -1,18 +1,20 @@
 class ReceiptProduct {
-  final String? name;
-  final String? totalPrice;
-  int? quantity;
-  String? pricePerUnit;
+  String name;
+  String totalPrice;
+  int quantity;
+  String pricePerUnit;
+  String discountCounted;
 
   ReceiptProduct({
-    this.name,
-    this.totalPrice,
+    this.name = 'Default receipt product name',
+    this.totalPrice = '0',
     this.quantity = 1,
     this.pricePerUnit = '',
+    this.discountCounted = '',
   });
 
   @override
   String toString() {
-    return 'Product{name: $name, totalPrice: $totalPrice, quantity: $quantity, pricePerUnit: $pricePerUnit}';
+    return 'Product{name: $name, totalPrice: $totalPrice, quantity: $quantity, pricePerUnit: $pricePerUnit, discountCounted: $discountCounted}';
   }
 }
