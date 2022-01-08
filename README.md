19bf41ac5c8fb668227a74dd501ec6fe9f3f4f1b
areee <10089872+areee@users.noreply.github.com>
2022-01-08 20:28:22 +0200

    Start implementing command line arguments

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # dart_kassakuitti_cli
 
-A simple Dart CLI app to handle a cash receipt coming from S-Kaupat (a Finnish food online store).
+A simple Dart CLI app to handle a cash receipt coming from S-kaupat or K-ruoka (two Finnish food online stores).
+
+
+## Usage
+
+You can define a cash receipt (t = text file), an EAN products file (h; html file) and which food online store to use (s).
+
+Let's suppose that you are in the root level of the project.
+
+```
+dart run bin/dart_kassakuitti_cli.dart -t assets/files/_cashReceipt.txt -h assets/files/_orderedProducts_S-kaupat.html -s S-kaupat
+```
--- bin/dart_kassakuitti_cli.dart
+++ bin/dart_kassakuitti_cli.dart
@@ -1,20 +1,41 @@
+import 'dart:io';
+import 'package:args/args.dart';
+
 import 'read_html_and_save_as_csv.dart';
 import 'read_receipt_and_save_as_csv.dart';
 import 'shop_selector_helper.dart';
 
+const textFile = 'text-file';
+const htmlFile = 'html-file';
+const foodOnlineStore = 'food-online-store';
+
 void main(List<String> arguments) {
-  const shopSelector =
-      ShopSelector.sKaupat; // TODO: make this a command line argument
+  exitCode = 0; // presume success
+  final parser = ArgParser()
+    ..addFlag(textFile, negatable: false, abbr: 't')
+    ..addFlag(htmlFile, negatable: false, abbr: 'h')
+    ..addFlag(foodOnlineStore, negatable: false, abbr: 's');
 
-  if (shopSelector == ShopSelector.sKaupat) {
-    readReceiptAndSaveAsCSV('assets/files/_cashReceipt.txt');
-    readHtmlAndSaveAsCSV(
-        'assets/files/_orderedProducts_S-kaupat.html', shopSelector);
-  } else {
-    // K-ruoka
-    readHtmlAndSaveAsCSV(
-        'assets/files/_orderedProducts_K-ruoka.html', shopSelector);
-  }
+  var argResults = parser.parse(arguments);
+  final paths = argResults.rest;
 
-  print('Done!');
+  print(argResults[textFile]);
+  print(argResults[htmlFile]);
+  print(argResults[foodOnlineStore]);
+  print(paths);
+
+  // const shopSelector =
+  //     ShopSelector.sKaupat; // TODO: make this a command line argument
+
+  // if (shopSelector == ShopSelector.sKaupat) {
+  //   readReceiptAndSaveAsCSV('assets/files/_cashReceipt.txt');
+  //   readHtmlAndSaveAsCSV(
+  //       'assets/files/_orderedProducts_S-kaupat.html', shopSelector);
+  // } else {
+  //   // K-ruoka
+  //   readHtmlAndSaveAsCSV(
+  //       'assets/files/_orderedProducts_K-ruoka.html', shopSelector);
+  // }
+
+  // print('Done!');
 }
