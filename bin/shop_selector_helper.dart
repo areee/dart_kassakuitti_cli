04c9100fead9f1ff654c2a6b62c11bb5ba2f1660
areee <10089872+areee@users.noreply.github.com>
2022-01-09 18:23:38 +0200

    Update ShopSelector (add extension)

--- bin/shop_selector_helper.dart
+++ bin/shop_selector_helper.dart
@@ -3,3 +3,20 @@ enum ShopSelector {
   sKaupat,
   kRuoka,
 }
+
+extension ShopSelectorExtension on ShopSelector {
+  static const values = {
+    ShopSelector.sKaupat: 'S-kaupat',
+    ShopSelector.kRuoka: 'K-ruoka',
+  };
+
+  String? get value => values[this];
+
+  bool isEqual(dynamic value) {
+    if (value is String) {
+      return toString() == value || this.value == value;
+    } else {
+      return false;
+    }
+  }
+}
