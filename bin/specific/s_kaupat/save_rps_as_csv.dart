3917608348e64840ae2a21d4e77bfdc817bfa382
areee <10089872+areee@users.noreply.github.com>
2022-04-02 09:59:17 +0300

    Update save_rps_as_csv (do not add discount counted column when all its rows are empty)

--- bin/specific/s_kaupat/save_rps_as_csv.dart
+++ bin/specific/s_kaupat/save_rps_as_csv.dart
@@ -6,11 +6,21 @@ import '../../models/receipt_product.dart';
 void receiptProducts2CSV(List<ReceiptProduct> products, String csvFilePath) {
   var csv = StringBuffer();
 
-  csv.write('Name;Quantity;Price per unit;Total price;Discount counted\n');
+  // If there aren't any discountCounted products, don't add it to the CSV file.
+  if (products.any((element) => element.discountCounted.isEmpty)) {
+    csv.write('Name;Quantity;Price per unit;Total price\n');
 
-  for (var product in products) {
-    csv.write(
-        '${product.name};${product.quantity};${product.pricePerUnit};${product.totalPrice};${product.discountCounted}\n');
+    for (var product in products) {
+      csv.write(
+          '${product.name};${product.quantity};${product.pricePerUnit};${product.totalPrice}\n');
+    }
+  } else {
+    csv.write('Name;Quantity;Price per unit;Total price;Discount counted\n');
+
+    for (var product in products) {
+      csv.write(
+          '${product.name};${product.quantity};${product.pricePerUnit};${product.totalPrice};${product.discountCounted}\n');
+    }
   }
 
   var file = File('$csvFilePath/receipt_products_${formattedDateTime()}.csv');
