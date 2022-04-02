18cefba937bc1acec78b53fbb9d54498a4249480
areee <10089872+areee@users.noreply.github.com>
2022-04-02 17:52:05 +0300

    Update price_helper (fix an error & enhance the output)

--- bin/utils/price_helper.dart
+++ bin/utils/price_helper.dart
@@ -1,5 +1,15 @@
 /// Count price per unit based on a product price and a quantity.
 String countPricePerUnit(String productPrice, int quantity) {
+  // In case the product price is empty (e.g. the last rows), return an empty string.
+  if (productPrice.isEmpty) {
+    return '';
+  }
+
+  // In case the quantity is 1, return an empty string (produces a cleaner CSV file).
+  if (quantity == 1) {
+    return '';
+  }
+
   var productPriceAsDouble =
       double.parse(productPrice.replaceAll(RegExp(r','), '.'));
   var pricePerUnit = productPriceAsDouble / quantity;
