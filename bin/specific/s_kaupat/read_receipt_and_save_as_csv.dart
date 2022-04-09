d5339c833b2036ecc99408221f8c4a39e50c9128
areee <10089872+areee@users.noreply.github.com>
2022-04-09 18:47:43 +0300

    Update readReceiptFile to use async-await

--- bin/specific/s_kaupat/read_receipt_and_save_as_csv.dart
+++ bin/specific/s_kaupat/read_receipt_and_save_as_csv.dart
@@ -3,17 +3,17 @@ import 'dart:io';
 import 'save_rps_as_csv.dart';
 import 'strings_to_products.dart';
 
-void readReceiptAndSaveAsCSV(String filePath, String csvFilePath) {
-  var lines = readReceiptFile(filePath);
+void readReceiptAndSaveAsCSV(String filePath, String csvFilePath) async {
+  var lines = await readReceiptFile(filePath);
   var products = strings2Products(lines ?? []);
   receiptProducts2CSV(products, csvFilePath);
 }
 
 /// Read a text file and return as a list of lines.
-List<String>? readReceiptFile(String filePath) {
+Future<List<String>?> readReceiptFile(String filePath) async {
   File file = File(filePath);
   try {
-    return file.readAsLinesSync();
+    return await file.readAsLines();
   } on FileSystemException {
     rethrow;
   }
