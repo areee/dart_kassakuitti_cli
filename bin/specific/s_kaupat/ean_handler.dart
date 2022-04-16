d02e336a88d557e53466c9249aacd9e1c39ed37e
areee <10089872+areee@users.noreply.github.com>
2022-04-16 09:36:46 +0300

    Update eanHandler (refactor & enhance a bit)

--- bin/specific/s_kaupat/ean_handler.dart
+++ bin/specific/s_kaupat/ean_handler.dart
@@ -8,64 +8,53 @@ void eanHandler(
   for (var receiptProduct in receiptProducts) {
     print(receiptProduct);
 
-    var filteredEanProducts = eanProducts
-        .where((eanProduct) =>
-            eanProduct.name.toLowerCase().contains(receiptProduct.name))
-        .toList();
+    var filteredEanProducts =
+        filterEANProducts(receiptProduct.name, eanProducts);
 
-    if (filteredEanProducts.length == 1) {
-      print('\tFound one product:');
-      print('\t\t${filteredEanProducts[0]}');
+    handleFoundCases(receiptProduct, filteredEanProducts);
 
-      receiptProduct.eanCode = filteredEanProducts[0].ean;
-    } else if (filteredEanProducts.length > 1) {
-      print('\tFound multiple products:');
-
-      for (var filteredReceiptProduct in filteredEanProducts) {
-        print('\t\t$filteredReceiptProduct');
-        print('\t\tIs this the right product? (y/n)');
-        var answer = stdin.readLineSync();
-        if (answer?.toLowerCase() == 'y') {
-          receiptProduct.eanCode = filteredReceiptProduct.ean;
-          break;
-        }
-      }
-    } else {
+    if (filteredEanProducts.isEmpty) {
       print('\tNo product found for the 1st round.');
-      // var splittedReceiptProcuctNames = receiptProcuctName.split(' ');
-      // var filteredEanProducts2 = eanProducts
-      //     .where((eanProduct) =>
-      //         eanProduct.name.contains(splittedReceiptProcuctNames[0]))
-      //     .toList();
+      var splittedReceiptProcuctNames = receiptProduct.name.split(' ');
 
-      // if (filteredEanProducts2.length == 1) {
-      //   print('\tFound one product:');
+      var filteredEanProducts =
+          filterEANProducts(splittedReceiptProcuctNames[0], eanProducts);
 
-      //   var filteredEanProduct = filteredEanProducts2[0];
-      //   receiptProduct.eanCode = filteredEanProduct.ean;
-      //   print('\t\t$filteredEanProduct');
+      handleFoundCases(receiptProduct, filteredEanProducts);
 
-      //   // print('\tLength of ean products 1: ${eanProducts.length}');
-      //   // eanProducts.remove(filteredEanProduct);
-      //   // print('\tLength of ean products 2: ${eanProducts.length}');
-      // } else if (filteredEanProducts2.length > 1) {
-      //   print('\tFound multiple products:');
+      if (filteredEanProducts.isEmpty) {
+        print('\tNo product found for the 2nd round.');
+      }
+    }
+  }
+}
 
-      //   for (var filteredReceiptProduct in filteredEanProducts2) {
-      //     print('\t\t$filteredReceiptProduct');
-      //     print('Did you mean this? (y/n)');
-      //     var answer = stdin.readLineSync();
-      //     if (answer?.toLowerCase() == 'y') {
-      //       receiptProduct.eanCode = filteredReceiptProduct.ean;
-      //       // print('\tLength of ean products 1: ${eanProducts.length}');
-      //       // eanProducts.remove(filteredReceiptProduct);
-      //       // print('\tLength of ean products 2: ${eanProducts.length}');
-      //       break;
-      //     }
-      //   }
-      // } else {
-      //   print('\tNo product found for the 2nd round.');
-      // }
+List<EANProduct> filterEANProducts(
+    String receiptProductName, List<EANProduct> eanProducts) {
+  return eanProducts
+      .where((eanProduct) =>
+          eanProduct.name.toLowerCase().contains(receiptProductName))
+      .toList();
+}
+
+void handleFoundCases(
+    ReceiptProduct receiptProduct, List<EANProduct> filteredEanProducts) {
+  if (filteredEanProducts.length == 1) {
+    print('\tFound one product:');
+    print('\t\t${filteredEanProducts[0]}');
+
+    receiptProduct.eanCode = filteredEanProducts[0].ean;
+  } else if (filteredEanProducts.length > 1) {
+    print('\tFound multiple products:');
+
+    for (var filteredReceiptProduct in filteredEanProducts) {
+      print('\t\t$filteredReceiptProduct');
+      print('\t\tIs this the right product? (y/n)');
+      var answer = stdin.readLineSync();
+      if (answer?.toLowerCase() == 'y') {
+        receiptProduct.eanCode = filteredReceiptProduct.ean;
+        break;
+      }
     }
   }
 }
