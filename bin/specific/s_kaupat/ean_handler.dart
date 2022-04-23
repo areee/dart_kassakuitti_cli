14f61d523a5c8e2a23865c709b53574062413820
areee <10089872+areee@users.noreply.github.com>
2022-04-23 19:13:00 +0300

    Update 'eanHandler' (better logic for selecting from multiple products)

--- bin/specific/s_kaupat/ean_handler.dart
+++ bin/specific/s_kaupat/ean_handler.dart
@@ -75,6 +75,7 @@ void _handleFoundCases(ReceiptProduct receiptProduct,
     AnsiPen pen = AnsiPen()
       ..black(bold: true)
       ..green(bold: true);
+
     print(pen('\tFound one product:'));
     print('\t\t${filteredEanProducts[0]}');
 
@@ -84,18 +85,30 @@ void _handleFoundCases(ReceiptProduct receiptProduct,
     AnsiPen pen = AnsiPen()
       ..black(bold: true)
       ..red(bold: true);
+
     print(pen('\tFound multiple products (${filteredEanProducts.length}'
         ' possible choices):'));
 
-    for (var filteredReceiptProduct in filteredEanProducts) {
-      print('\t\t$filteredReceiptProduct');
-      print('\t\tIs this the right product? (y/n)');
-      var answer = stdin.readLineSync();
-      if (answer?.toLowerCase() == 'y') {
-        receiptProduct.eanCode = filteredReceiptProduct.ean;
-        origEanProducts.remove(filteredReceiptProduct);
-        break;
-      }
+    for (var i = 0; i < filteredEanProducts.length; i++) {
+      print('\t\t${i + 1}. ${filteredEanProducts[i]}');
+    }
+    print('\nPlease enter the number of the product you want to select: ');
+    var answer = stdin.readLineSync();
+
+    if (answer!.isNotEmpty &&
+        int.parse(answer) <= filteredEanProducts.length &&
+        int.parse(answer) > 0) {
+      AnsiPen pen = AnsiPen()
+        ..black(bold: true)
+        ..green(bold: true);
+
+      print(
+          pen('\tYou selected: ${filteredEanProducts[int.parse(answer) - 1]}'));
+
+      receiptProduct.eanCode = filteredEanProducts[int.parse(answer) - 1].ean;
+      origEanProducts.remove(filteredEanProducts[int.parse(answer) - 1]);
+    } else {
+      print('\tNo product selected.');
     }
   }
 }
