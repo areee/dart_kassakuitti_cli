36a505de9a1ea2bfae3c6a4012628f09228d8cc7
areee <10089872+areee@users.noreply.github.com>
2022-11-26 19:37:58 +0200

    Add some new methods for StringExtension

--- bin/utils/extensions/string_extension.dart
+++ bin/utils/extensions/string_extension.dart
@@ -7,6 +7,19 @@ extension StringExtension on String? {
 
   /// Checks if the string is not null or empty.
   bool isNotNullOrEmpty() {
-    return this != null && this!.isNotEmpty;
+    return !isNullOrEmpty();
+  }
+
+  /// Parses the string to a double.
+  double? toDoubleOrNull() {
+    if (isNullOrEmpty()) return null;
+    return double.tryParse(this!.replaceAll(',', '.'));
+  }
+
+  /// Checks if the string is EAN-13.
+  bool isEan13() {
+    if (isNullOrEmpty()) return false;
+    if (this!.length == 13 && RegExp(r'^\d{13}$').hasMatch(this!)) return true;
+    return false;
   }
 }
